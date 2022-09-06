2861a1bebf45b1d84d086405e08e3d36f1154874
Sayedul Karim <100830568+sayedulkrm@users.noreply.github.com>
2022-09-06 12:10:06 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require ('express');
+const path = require ('path');
+const bodyParser = require ('body-parser');
+const http = require ('http');
+const predictRouter = require('./routes/predict');
+
+const app = express();
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/hello', function(req, res) {
+    res.send('hello')
+});
+app.use('/predict', predictRouter);
+app.get('/', function(req, res) {
+    return res.sendFile(path.join(__dirname, "build", "index.html"))
+})
+const port = process.env.PORT || '8080';
+
+
+app.set('port', port);
+const server = http.createServer(app);
+server.listen(port);
+server.on('listening', () => {
+    console.log('Listening on ' + (port));
+});
